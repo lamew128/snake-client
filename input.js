88d19bad9d42bef0f4d2b9a988258d21b05dc33b
Eugene <lamew128@gmail.com>
2022-04-13 15:31:46 -0400

    input js

---
+++ input.js
@@ -0,0 +1,25 @@
+const handleUserInput = function (key) {
+  // your code here
+  if (key === '\u0003') {
+;    process.exit();
+  } else if (key === 'W' || key ==='w') {
+
+  } else if (key === 'A' || key ==='a') {
+    
+  } else if (key === 'S' || key ==='s') {
+    
+  } else if (key === 'D' || key ==='d') {
+    
+  }
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+module.exports = {setupInput};
