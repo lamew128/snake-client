e89900ea383deaa33260857bef410865e538c31d
Eugene <lamew128@gmail.com>
2022-04-13 15:45:40 -0400

    wasd

--- input.js
+++ input.js
@@ -1,19 +1,21 @@
+let connection;
 const handleUserInput = function (key) {
   // your code here
   if (key === '\u0003') {
-;    process.exit();
+      process.exit();
   } else if (key === 'W' || key ==='w') {
-
+    connection.write("Move: up");
   } else if (key === 'A' || key ==='a') {
-    
+    connection.write("Move: left");
   } else if (key === 'S' || key ==='s') {
-    
+    connection.write("Move: down");
   } else if (key === 'D' || key ==='d') {
-    
+    connection.write("Move: right");
   }
 };
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
