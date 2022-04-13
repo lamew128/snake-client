d790c134fd35d004617e1e7a8e8ecdf165e3d6de
Eugene <lamew128@gmail.com>
2022-04-13 15:55:46 -0400

    constant

--- client.js
+++ client.js
@@ -1,8 +1,9 @@
 const net = require("net");
+const {IP, PORT} = require("./constants");
 const connect = function () {
   const conn = net.createConnection({
-    host: '165.227.47.243',// IP address here,
-    port: '50541'// PORT number here,
+    host: IP,// IP address here,
+    port: PORT// PORT number here,
   });
 
   // interpret incoming data as text
@@ -11,7 +12,6 @@ const connect = function () {
   conn.on("connect", () => {
     conn.write("EWZ has connected");
     conn.write("Name: EWZ");
-    //setInterval(() => (conn.write("Move: up")), 1000);
   });
 
   conn.on("data", (data) => {
